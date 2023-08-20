a1db142722781c369f0df4539bdd98808172e866
unknown <blaiseirakoze50@gmail.com>
2023-08-20 15:05:52 +0200

    changes

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,11 +5,11 @@ dotenv.config();
 const config =
     {
       development : {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: "mariadb",
+        username: 'admin',
+        password: 'admin@123',
+        database: 'approval_db',
+        host: 'localhost',
+        dialect: "mysql",
         logging:true
       },
       test: {
@@ -17,14 +17,14 @@ const config =
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME_TEST,
         host: process.env.DB_HOST,
-        dialect: "mariadb"
+        dialect: "mysql"
       },
       production: {
         username: process.env.DB_USERNAME_PRO,
-        password: process.env.DB_PASSWORD_PRO,
+        // password: process.env.DB_PASSWORD_PRO,
         database: process.env.DB_NAME_PRO,
         host: process.env.DB_HOST,
-        dialect: "mariadb"
+        dialect: "mysql"
       }
     }
 module.exports = config;
